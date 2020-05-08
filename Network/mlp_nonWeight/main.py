60d710c897068f4cbbf91f903eaa9ca69920c304
林杰 <linjie@linjiedeMacBook-Pro.local>
2020-05-08 12:14:04 +0800

    整理文件夹

--- Network/mlp_nonWeight/main.py
+++ Network/mlp_nonWeight/main.py
@@ -4,13 +4,11 @@ from MLP_utils import _test, _train
 
 from torch.utils.data import DataLoader
 
-
-
 if __name__ == '__main__':
     linear_net = LinearNet([18, 32, 64, 32, 3])
     
-    train_data = agentData('./data', 6)
-    test_data = agentData('./data', 6)
+    train_data = agentData('../data', 6)
+    test_data = agentData('../data', 6)
 
     train_dl = DataLoader(train_data, 2, shuffle=False)
     test_dl = DataLoader(test_data, 1, shuffle=True)
